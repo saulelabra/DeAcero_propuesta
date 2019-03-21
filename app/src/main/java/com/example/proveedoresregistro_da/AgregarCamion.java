86ef262d4e257e0753137a36f0cdfa7a7712c8b8
saulelabra <quiquelabra@gmail.com>
2019-03-21 11:55:00 -0600

    update

--- app/src/main/java/com/example/proveedoresregistro_da/AgregarCamion.java
+++ app/src/main/java/com/example/proveedoresregistro_da/AgregarCamion.java
@@ -5,16 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class AgregarChofer extends AppCompatActivity {
+public class AgregarCamion extends AppCompatActivity {
 
-    public void guardarYReg(View view) {
-        Intent toMenu = new Intent(AgregarChofer.this, Menu.class);
+    public void guardarYReg(View view)
+    {
+        Intent toMenu = new Intent(AgregarCamion.this, Menu.class);
         startActivity(toMenu);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_agregar_chofer);
+        setContentView(R.layout.activity_agregar_camion);
     }
 }
--- app/src/main/java/com/example/proveedoresregistro_da/Menu.java
+++ app/src/main/java/com/example/proveedoresregistro_da/Menu.java
@@ -1,9 +1,21 @@
 package com.example.proveedoresregistro_da;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu extends AppCompatActivity {
+
+    public void regChofer(View view) {
+        Intent toRegChofer = new Intent(Menu.this, AgregarChofer.class);
+        startActivity(toRegChofer);
+    }
+
+    public void regCamion(View view) {
+        Intent toRegCamion = new Intent(Menu.this, AgregarCamion.class);
+        startActivity(toRegCamion);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
