418668be66a22d00da63637152512c43c59fd41e
saulelabra <quiquelabra@gmail.com>
2019-03-26 21:56:38 -0600

    Update - Se agregaron los puntos de la presentación

--- app/src/main/java/com/example/proveedoresregistro_da/AgregarEnvio.java
+++ app/src/main/java/com/example/proveedoresregistro_da/AgregarEnvio.java
@@ -1,9 +1,16 @@
 package com.example.proveedoresregistro_da;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AgregarEnvio extends AppCompatActivity {
+
+    public void guardarEnvioYReg(View view) {
+        Intent goToMenu = new Intent(AgregarEnvio.this, Menu.class);
+        startActivity(goToMenu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/proveedoresregistro_da/AgregarTransportista.java
+++ app/src/main/java/com/example/proveedoresregistro_da/AgregarTransportista.java
@@ -1,9 +1,16 @@
 package com.example.proveedoresregistro_da;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AgregarTransportista extends AppCompatActivity {
+
+    public void goToMenu(View view){
+        Intent toMenu = new Intent(AgregarTransportista.this, Menu.class);
+        startActivity(toMenu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
