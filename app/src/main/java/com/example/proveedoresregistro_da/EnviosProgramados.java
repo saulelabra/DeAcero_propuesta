fba506964f4cce5db28bf7bb51da1054f4d649a4
saulelabra <quiquelabra@gmail.com>
2019-04-04 15:55:46 -0600

    update

---
+++ app/src/main/java/com/example/proveedoresregistro_da/EnviosProgramados.java
@@ -0,0 +1,13 @@
+package com.example.proveedoresregistro_da;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnviosProgramados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_envios_programados);
+    }
+}
