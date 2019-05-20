eea90f47e81ff07f42c297a0042c5453326c7aa0
RodrigoGM97 <rodrigogm971@gmail.com>
2019-05-20 07:14:18 -0500

    Ya se pueden agregar transportistas

--- app/src/main/java/com/example/proveedoresregistro_da/AgregarTransportista.java
+++ app/src/main/java/com/example/proveedoresregistro_da/AgregarTransportista.java
@@ -1,21 +1,38 @@
 package com.example.proveedoresregistro_da;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class AgregarTransportista extends AppCompatActivity {
 
+    EditText nom, tel, correo;
+    String nom_str, tel_str, correo_str;
+    String url_add_transportista = "http://ubiquitous.csf.itesm.mx/~pddm-1020725/content/DeAcero_API/queries/insert.transportista.php?";
+    Button savebtn;
+
     public void fillName(View view){
-        EditText nombre = findViewById(R.id.nombre);
-        nombre.setText(recuperarNombreUsuario());
+        nom.setText(recuperarNombreUsuario());
     }
 
     public void goToMenu(View view){
@@ -28,7 +45,62 @@ public class AgregarTransportista extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_transportista);
 
+        nom = findViewById(R.id.nombre);
+        tel = findViewById(R.id.tel);
+        correo = findViewById(R.id.correo);
+        savebtn = findViewById(R.id.save_btn);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        savebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nom_str = nom.getText().toString();
+                tel_str = tel.getText().toString();
+                correo_str = correo.getText().toString();
+
+                guardarTransportista();
+            }
+        });
+
+    }
+
+    private void guardarTransportista()
+    {
+        final ProgressDialog barraDeProgreso = new ProgressDialog(AgregarTransportista.this);
+        barraDeProgreso.setMessage("Cargando");
+        barraDeProgreso.show();
+        String imageData = "http://ubiquitous.csf.itesm.mx/~pddm-1024595/content/proyecto/prueba_img/credencial-actual.jpg";
+
+        url_add_transportista = url_add_transportista + "nombre="+
+                nom_str+ "&tel=" + tel_str + "&correo=" + correo_str;
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url_add_transportista, null, new Response.Listener<JSONArray>() {
+            public void onResponse(JSONArray response) {
+                barraDeProgreso.hide();
+                Toast.makeText(AgregarTransportista.this, "Datos ingresados", Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                barraDeProgreso.hide();
+                Toast.makeText(AgregarTransportista.this, "Error en: " + error.toString(), Toast.LENGTH_LONG).show();
+            }
+        }) {
+            //getHeaders() se ejecuta automáticamente en cuanto se ejecuta la actividad
+            @Override public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+
+                //Hasheando desde el servidor (php) cifrado para el acceso de la carpeta
+                String credenciales = "Basic YTAxMDIwNzI1OjAwMDA=";
+                headers.put("Content-Type", "application/json");
+                headers.put("Authorization", credenciales);
+
+                return headers;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
     }
 
     public String recuperarNombreUsuario()
